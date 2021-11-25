d8641578890d49d84a8705e62b8efa7b20b4e540
pipeleon <oflg94@gmail.com>
2021-11-24 16:30:45 -0800

    Task 0 corrected

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -12,5 +12,24 @@ void display_complex_number(complex c)
 	if (c.im == 0)
 		printf("%.0f\n", c.re);
 	else
-		printf("%.0f + %.0fi\n", c.re, c.im);
+	{
+		if (c.im < 0)
+		{
+			if (c.im == -1)
+				printf("%.0f - i\n", c.re);
+			else
+			{
+				c.im = c.im * (-1);
+				printf("%.0f - %.0fi\n", c.re, c.im);
+			}
+		}
+		else
+		{
+			if (c.im == 1)
+				printf("%.0f - i\n", c.re);
+			else
+				printf("%.0f + %.0fi\n", c.re, c.im);
+
+		}
+	}
 }

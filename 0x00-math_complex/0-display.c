61fa3cf52de189b78eb48e9dad2f6e09a2df0d5c
pipeleon <oflg94@gmail.com>
2021-11-24 16:23:47 -0800

    Task 0 and Readme

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,7 +10,7 @@
 void display_complex_number(complex c)
 {
 	if (c.im == 0)
-		printf("%.0lf\n", c.re);
+		printf("%.0f\n", c.re);
 	else
-		printf("%.0lf + %.0lfi\n", c.re, c.im);
+		printf("%.0f + %.0fi\n", c.re, c.im);
 }
